a31008338b543e513a7af40535ceb29c6cc74238
cogpark <cogpark@gmail.com>
2019-11-09 16:33:32 -0500

    Example pages.

--- app/routes.py
+++ app/routes.py
@@ -10,3 +10,16 @@ def index():
 def testpage():
     return render_template('testpage.html')
 
+@app.route('/conditionaleg')
+def conditionaleg():
+    title = 'This is a test title'
+    return render_template('conditionaleg.html', title=title)
+
+@app.route('/testloop')
+def testloop():
+    posts = [
+        {'title': 'Test post 1'},
+        {'title': 'Test post 2'},
+        {'title': 'Test post 3'}
+    ]
+    return render_template('testloop.html', title='Posts', posts=posts)
