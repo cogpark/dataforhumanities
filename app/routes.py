5990c35334d97f7d7422c81f695757bb8e252d05
cogpark <cogpark@gmail.com>
2019-11-11 13:37:44 -0500

    Draft of vlookups.

--- app/routes.py
+++ app/routes.py
@@ -6,14 +6,25 @@ from flask import render_template
 def index():
     return render_template('index.html', title='Home')
 
+@app.route('/basicjoins')
+def basicjoins():
+    return render_template('basicjoins.html')
+
+@app.route('/vlookups')
+def vlookups():
+    return render_template('vlookups.html')
+
+# Test pages
 @app.route('/testpage')
 def testpage():
     return render_template('testpage.html')
+
 
 @app.route('/conditionaleg')
 def conditionaleg():
     title = 'This is a test title'
     return render_template('conditionaleg.html', title=title)
+
 
 @app.route('/testloop')
 def testloop():
@@ -23,3 +34,4 @@ def testloop():
         {'title': 'Test post 3'}
     ]
     return render_template('testloop.html', title='Posts', posts=posts)
+
